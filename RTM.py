4f59e2f9ca8817035a66063eb0cf0d700c53cadf
RAIHAN-CYBER-BBZ <126344140+RAIHAN-CYBER-BBZ@users.noreply.github.com>
2023-02-25 21:38:22 +0400

    Update RTM.py

--- RTM.py
+++ RTM.py
@@ -1,1 +1,9 @@
-import RR
+import os,time,platform
+os.system('clear')
+bit = platform.architecture()[0]
+if bit=='64bit':
+    import RR
+else:
+    print('\033[1;31m[×] Sorry Device Not Support')
+ 
+ 
